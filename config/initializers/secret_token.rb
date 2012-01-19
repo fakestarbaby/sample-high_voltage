c14c5884d8b0fa4a86f3f20a43bdf31d6e781229
fakestarbaby <fakestarbaby@gmail.com>
2012-01-19 20:06:32 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleHighVoltage::Application.config.secret_token = '9fa8d9c37819063f60028635a96c0aa385ea1b40cc06eadd584254783a202d929e06ac404da38a8953bd535b3256076bc7bc24c8f17b989d88f488e8a9a7a5ad'
